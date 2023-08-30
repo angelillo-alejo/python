856f1cd0f72d95da440cf15e45c981891c62e001
angelillo-alejo <78606216+angelillo-alejo@users.noreply.github.com>
2023-08-30 13:31:27 +0200

    add list

--- 02_operators.py
+++ 02_operators.py
@@ -23,7 +23,22 @@ print(3<=4)
 print(3==4)
 print(3!=4)
 
+# Operaciones con cadenas de texto
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
 
+# Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
 
 
 
